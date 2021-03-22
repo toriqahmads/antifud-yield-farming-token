3e4ee9b7e75f03dbf7060db965ad99b7e5c21d43
blzd-dev <blizzard.money.dev@gmail.com>
2021-03-22 21:30:21 +0700

    remove goose twitter card

--- src/views/Home/components/TwitterCard.tsx
+++ src/views/Home/components/TwitterCard.tsx
@@ -35,7 +35,7 @@ const TwitterCard = () => {
         <Timeline
           dataSource={{
             sourceType: 'profile',
-            screenName: 'GooseFinance'
+            screenName: 'Blizzard.money'
           }}
           options={{
             height: '300',
